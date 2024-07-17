bee3586853ce54a8dfd67a2a5a3e12f1a2f61d2c
tokisuno <lucaswmei@gmail.com>
2024-07-17 11:15:49 -0400

    find_mapping function implementation

--- lua/stackmap/init.lua
+++ lua/stackmap/init.lua
@@ -8,22 +8,48 @@ local M = {}
 --  - vim.keymap.set(...)           -> create new keymaps
 --  - vim.api.nvim_get_keymap(...)  -> gets keymaps
 
-M.push = function (name, mappings)
+-- lhs -> left-hand side
+local find_mapping = function (maps, lhs)
+  -- pairs
+  --  - iterates over EVERY key in a table
+  --  - order not guaranteed
+  --  - used for string keys
+  -- ipairs
+  --  - iterate over numeric keys
+  --  - order IS guaranteed
 
+  for _, value in ipairs(maps) do
+    if value.lhs == lhs then
+      return value
+    end
+  end
+end
+
+M.push = function (name, mode, mappings)
+  local maps = vim.api.nvim_get_keymap(mode)
+
+  P(find_mapping(maps, "<leader>ph"))
+  -- P(maps)
 end
 
 M.pop = function (name)
 
 end
 
+M.push("debug_mode", "n", {
+  ["<leader>ph"] = "echo 'hello'",
+  ["<leader>pz"] = "echo 'goodbye'",
+})
+
 --[[
-lua require('mapstack').push('debug_mode', {
-  ... mappings in here
+
+lua require('mapstack').push('debug_mode', 'n' {
 })
 
 ...
 
 lua require('mapstack').pop('debug_mode')
+
 --]]
 
 return M
