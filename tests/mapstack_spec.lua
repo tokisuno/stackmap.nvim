794f6678ac6dfea0f6810215d7363854102f333d
tokisuno <lucaswmei@gmail.com>
2024-07-18 09:07:49 -0400

    finished tests

--- tests/mapstack_spec.lua
+++ tests/mapstack_spec.lua
@@ -6,7 +6,7 @@ local find_map = function(maps, lhs)
   end
 end
 
-describe("mapstack", function()
+describe("stackmap", function()
 	it("can be required", function()
 		require("stackmap")
 	end)
@@ -22,5 +22,20 @@ describe("mapstack", function()
     -- once the function has done its thing:
     --  assert that the right behaviour occurs
     assert.are.same(rhs, found.rhs)
+  end)
+
+  it('can push multiple mappings', function ()
+    local rhs = "echo 'this is a test'"
+    require("stackmap").push("test1", "n", {
+      ["asdf_1"] = rhs .. "1",
+      ["asdf_2"] = rhs .. "2",
+    })
+    local maps = vim.api.nvim_get_keymap('n')
+
+    local found_1 = find_map(maps, "asdf_1")
+    assert.are.same(rhs .. "1", found_1.rhs)
+
+    local found_2 = find_map(maps, "asdf_2")
+    assert.are.same(rhs .. "2", found_2.rhs)
   end)
 end)
